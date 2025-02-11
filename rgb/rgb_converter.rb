66b84932bfd51639aa49f6ede11bfb3a50e8a5f6
shou-317 <shou6130@gmail.com>
2025-02-11 11:27:44 +0900

    refactor: rubocop

--- rgb/rgb_converter.rb
+++ rgb/rgb_converter.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # 10進数を16進数に変換
 def to_hex(*rgb)
-  rgb.map { |int| int.to_s(16).rjust(2, "0") }.prepend("#").join
+  rgb.map { |int| int.to_s(16).rjust(2, '0') }.prepend('#').join
 end
 
 # 16進数を10進数に変換
--- rgb/test_rgb_converter.rb
+++ rgb/test_rgb_converter.rb
@@ -3,6 +3,7 @@
 require 'minitest/autorun'
 require_relative 'rgb_converter'
 
+# test rgb_converter
 class RgbConverterTest < Minitest::Test
   def test_to_hex
     assert_equal '#000000', to_hex(0, 0, 0)
