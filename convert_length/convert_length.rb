a0f5ef1da00136f3aec2784eb1570ae145f9fa0b
shou-317 <shou6130@gmail.com>
2025-02-12 11:40:33 +0900

    refactor: Change arguments to keyword arguments

--- convert_length/convert_length.rb
+++ convert_length/convert_length.rb
@@ -2,6 +2,6 @@
 
 UNIT = { m: 1.00, ft: 3.28, in: 39.37 }.freeze
 # m換算した後、指定の単位に変換する
-def convert_length(num, from, to)
+def convert_length(num, from: :m, to: :m)
   (num / UNIT[from] * UNIT[to]).round(2)
 end
