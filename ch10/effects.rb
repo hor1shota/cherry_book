9b417eb21502b1bf01109de2763bbc2f23657c4d
shou-317 <shou6130@gmail.com>
2025-02-16 17:10:07 +0900

    feat: implementation of Effects.#echo

--- ch10/effects.rb
+++ ch10/effects.rb
@@ -2,4 +2,21 @@ module Effects
   def self.reverse
     ->(words) { words.split(' ').map(&:reverse).join(' ') }
   end
+
+  def self.echo(cnt)
+    ->(words) do words
+      .split(' ')
+      .map { |word| repeat_chars(word, cnt) }
+      .join(' ')
+    end
+  end
+
+  def self.repeat_chars(word, cnt)
+    word
+      .chars
+      .map { |char| char * cnt }
+      .join
+  end
+
+  private_class_method :repeat_chars
 end
